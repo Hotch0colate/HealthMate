f21b07f5b7b5443dea1fa4dd2adee9fea6b68d4f
Gjittapat <jittapat.chan@kmutt.ac.th>
2024-02-20 13:23:54 +0700

    clean up comments and structures

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const FirstLogin1(),
+      home: const SignupPage(),
       routes: {
         '/login': (context) => LoginPage(),
         '/signup': (context) => SignupPage(),
