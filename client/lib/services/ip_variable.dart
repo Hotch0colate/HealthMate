df7ea59e7db8d3b82f25ef1e3ea2e8489b9f16f6
Chaninat Phongam <114181275+ferncnn46@users.noreply.github.com>
2024-04-18 20:29:07 +0700

    first commit

--- client/lib/services/ip_variable.dart
+++ client/lib/services/ip_variable.dart
@@ -1,8 +1,9 @@
 library globals;
 
 //Config according to your IP
-String fixedIp = "192.168.1.121";
+String fixedIp = "192.168.1.38";
 
 //Phat IP Home 192.168.1.3
 
 //Hor Pong IP 192.168.1.121
+//Fern IP Home 192.168.1.3
